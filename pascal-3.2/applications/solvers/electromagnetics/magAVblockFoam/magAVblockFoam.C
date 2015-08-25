078985c05339cd1a72d2b9c67d41f02ef3ee9767
Pascal Beckstein <pascal@becksteins.de>
2015-08-25 16:48:30 +0200

    Update foam-extend to nextRelease and prepare for NUMAP-FOAM
    - Added magAVblockFoam solver
    - Added simple 3D example tutorial for the magAVblockFoam solver

---
+++ pascal-3.2/applications/solvers/electromagnetics/magAVblockFoam/magAVblockFoam.C
@@ -0,0 +1,125 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | foam-extend: Open Source CFD
+   \\    /   O peration     |
+    \\  /    A nd           | For copyright notice see file Copyright
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of foam-extend.
+
+    foam-extend is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    foam-extend is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with foam-extend.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    ...
+
+Description
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "fieldTypes.H"
+#include "foamTime.H"
+#include "fvMesh.H"
+#include "fvBlockMatrix.H"
+
+#include "blockMatrixTools.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+
+#   include "setRootCase.H"
+#   include "createTime.H"
+#   include "createMesh.H"
+
+#   include "createFields.H"
+
+    using namespace Foam;
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+    
+    while (runTime.run())
+    {
+        runTime++;
+
+        Info << "Time = " << runTime.value() << nl << endl;
+        
+        // ==================================================================//
+        // Constants
+        // ==================================================================//
+
+        scalar pi = mathematicalConstant::pi;
+
+        dimensionedScalar mu0
+        (
+            "mu0",
+            dimensionSet(1, 1, -2, 0, 0, -2, 0),
+            4.0 * pi * 1e-07
+        );
+
+        dimensionedScalar rMu0 = 1.0/mu0;
+
+        // ==================================================================//
+        // Omega and alpha
+        // ==================================================================//
+
+        dimensionedScalar omega
+        (
+            "omega",
+            2.0 * pi * frequency
+        );
+        
+        dimensionedScalar rOmega = 1.0/omega;
+
+        volScalarField alpha = omega * sigma;
+
+        // ==================================================================//
+        // Solve quasi-static Maxwell-Equations for low Rm
+        // ==================================================================//
+
+#       include "ABlockEqn.H"
+// #       include "VEqn.H"
+
+        // ==================================================================//
+        // Calculate derived fields
+        // ==================================================================//
+
+        jRe ==  alpha * AIm;
+        jIm == -alpha * ARe;
+
+        BRe == fvc::curl(ARe);
+        BIm == fvc::curl(AIm);
+
+        FL == 0.5 * ( (jRe ^ BRe) + (jIm ^ BIm) );
+
+        pB == 0.5 * rMu0
+        * 0.5 * ( (BRe & BRe) + (BIm & BIm) );
+
+        runTime.write();
+
+        Info << "ExecutionTime = "
+            << scalar(runTime.elapsedCpuTime()) << " s"
+            << nl << endl << endl;
+    }
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    Info << "End" << nl << endl;
+
+    return(0);
+}
+
+// ************************************************************************* //
