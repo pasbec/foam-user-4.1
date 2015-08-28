3e3db342e3fc55acd021d79c92fd4bce1be8c1d5
Pascal Beckstein <pascal@becksteins.de>
2015-08-28 14:18:59 +0200

    Bugfixes and work-around for diagonal-problem with block-matrices

--- pascal-3.2/applications/solvers/electromagnetics/magAVblockFoam/magAVblockFoam.C
+++ pascal-3.2/applications/solvers/electromagnetics/magAVblockFoam/magAVblockFoam.C
@@ -46,7 +46,7 @@ int main(int argc, char *argv[])
 #   include "createFields.H"
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
-        
+
     // Constants, Frequency and Alpha
 
     dimensionedScalar mu0
@@ -63,39 +63,39 @@ int main(int argc, char *argv[])
         "omega",
         2.0 * mathematicalConstant::pi * frequency
     );
-    
+
     dimensionedScalar rOmega = 1.0/omega;
 
     volScalarField alpha = omega * sigma;
-        
+
     while (runTime.run())
     {
         runTime++;
 
         Info << "Time = " << runTime.value() << nl << endl;
-        
+
         // ==================================================================//
         // Solve quasi-static Maxwell-Equations for low Rm
         // ==================================================================//
-        
+
         // Prepare block system
         fvBlockMatrix<vector10> AVEqn(AV);
-        
+
         // Assembleand insert equations for A
 #       include "AEqn.H"
-        
+
         AVEqn.insertEquation(0, AReEqn);
         AVEqn.insertEquation(5, AImEqn);
-        
+
         // Assembleand insert equations for divA
 #       include "divAEqn.H"
-        
+
         AVEqn.insertEquation(3, divAReEqn);
         AVEqn.insertEquation(8, divAImEqn);
 
         // Assemble and insert equations for V
 #       include "VEqn.H"
-        
+
         AVEqn.insertEquation(4, VReEqn);
         AVEqn.insertEquation(9, VImEqn);
 
@@ -111,13 +111,13 @@ int main(int argc, char *argv[])
 
         ARe.correctBoundaryConditions();
         AIm.correctBoundaryConditions();
-        
+
         AVEqn.retrieveSolution(3, divARe.internalField());
         AVEqn.retrieveSolution(8, divAIm.internalField());
 
         ARe.correctBoundaryConditions();
         AIm.correctBoundaryConditions();
-        
+
         AVEqn.retrieveSolution(4, VRe.internalField());
         AVEqn.retrieveSolution(9, VIm.internalField());
 
@@ -142,45 +142,45 @@ int main(int argc, char *argv[])
         // ==================================================================//
         // Debug stuff
         // ==================================================================//
-        
+
         surfaceScalarField AReFlux ("AReFlux", fvc::interpolate(ARe) & mesh.Sf());
         surfaceScalarField AImFlux ("AImFlux", fvc::interpolate(AIm) & mesh.Sf());
-        
+
         volScalarField AReMag ("AReMag", mag(ARe)); AReMag.write();
         volScalarField AImMag ("AImMag", mag(AIm)); AImMag.write();
-        
+
         volScalarField BReMag ("BReMag", mag(BRe)); BReMag.write();
         volScalarField BImMag ("BImMag", mag(BIm)); BImMag.write();
-        
+
         surfaceScalarField jReFlux ("AReFlux", fvc::interpolate(jRe) & mesh.Sf());
         surfaceScalarField jImFlux ("AImFlux", fvc::interpolate(jIm) & mesh.Sf());
-        
+
         volScalarField jReMag ("jReMag", mag(jRe)); jReMag.write();
         volScalarField jImMag ("jImMag", mag(jIm)); jImMag.write();
-        
+
         volScalarField jReDiv ("jReDiv", fvc::div(jRe)); jReDiv.write();
         volScalarField jImDiv ("jImDiv", fvc::div(jIm)); jImDiv.write();
-        
+
         volScalarField jsReMag ("jsReMag", mag(jsRe)); jsReMag.write();
         volScalarField jsImMag ("jsImMag", mag(jsIm)); jsImMag.write();
-        
+
         faceSet conductorFaces(mesh, "faceSet_geometry_conductor");
-        
+
         scalarField conductorSigmaf(conductorFaces.size(),0.0);
         scalarField conductorAlphaAReFlux(conductorFaces.size(),0.0);
         scalarField conductorAlphaAImFlux(conductorFaces.size(),0.0);
         scalarField conductorJReFlux(conductorFaces.size(),0.0);
         scalarField conductorJImFlux(conductorFaces.size(),0.0);
-        
+
         forAll(conductorFaces, faceI)
         {
             label faceLabel = conductorFaces[faceI];
-            
+
             conductorSigmaf[faceI] = sigmaf[faceLabel];
-            
+
             conductorAlphaAReFlux[faceI] = omega.value() * sigmaf[faceI] * AReFlux[faceI];
             conductorAlphaAImFlux[faceI] = omega.value() * sigmaf[faceI] * AImFlux[faceI];
-            
+
             conductorJReFlux[faceI] = jReFlux[faceLabel];
             conductorJImFlux[faceI] = jImFlux[faceLabel];
         }
@@ -188,16 +188,26 @@ int main(int argc, char *argv[])
         // ==================================================================//
         // Debug output
         // ==================================================================//
-        
+
+        Info << "divAReEqn.hasDiag()" << " = " << divAReEqn.hasDiag() << endl;
+        Info << "divAReEqn.hasLower()" << " = " << divAReEqn.hasLower() << endl;
+        Info << "divAReEqn.hasUpper()" << " = " << divAReEqn.hasUpper() << endl;
+        Info << "divAReEqn.diagonal()" << " = " << divAReEqn.diagonal() << endl;
+
+        Info << "divAImEqn.hasDiag()" << " = " << divAImEqn.hasDiag() << endl;
+        Info << "divAImEqn.hasLower()" << " = " << divAImEqn.hasLower() << endl;
+        Info << "divAImEqn.hasUpper()" << " = " << divAImEqn.hasUpper() << endl;
+        Info << "divAImEqn.diagonal()" << " = " << divAImEqn.diagonal() << endl;
+
         Info << "max(mag(jReDiv)) = " << max(mag(jReDiv)) << endl;
         Info << "max(mag(jImDiv)) = " << max(mag(jImDiv)) << endl;
-        
+
         Info << "gMax(conductorSigmaf) = " << gMax(conductorSigmaf) << endl;
         Info << "gMax(mag(conductorAlphaAReFlux)) = " << gMax(mag(conductorAlphaAReFlux)) << endl;
         Info << "gMax(mag(conductorAlphaAImFlux)) = " << gMax(mag(conductorAlphaAImFlux)) << endl;
         Info << "gMax(mag(conductorJReFlux)) = " << gMax(mag(conductorJReFlux)) << endl;
         Info << "gMax(mag(conductorJImFlux)) = " << gMax(mag(conductorJImFlux)) << endl;
-        
+
         // ==================================================================//
         // Write
         // ==================================================================//
