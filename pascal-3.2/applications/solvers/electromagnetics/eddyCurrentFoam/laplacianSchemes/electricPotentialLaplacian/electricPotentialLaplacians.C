e44ab1c1859f114a134b74f88f3109913562dfb7
Pascal Beckstein <pascal@becksteins.de>
2015-10-20 15:01:23 +0200

    Implement electricPotentialLaplacian
    - Clone of gaussLaplacianScheme
    - Renamed to electricPotential
    - Additional data for upcoming changes like Named Enum, magneticProperties, etc

---
+++ pascal-3.2/applications/solvers/electromagnetics/eddyCurrentFoam/laplacianSchemes/electricPotentialLaplacian/electricPotentialLaplacians.C
@@ -0,0 +1,180 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | foam-extend: Open Source CFD
+   \\    /   O peration     | Version:     3.2
+    \\  /    A nd           | Web:         http://www.foam-extend.org
+     \\/     M anipulation  | For copyright notice see file Copyright
+-------------------------------------------------------------------------------
+License
+    This file is part of foam-extend.
+
+    foam-extend is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    foam-extend is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with foam-extend.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "electricPotentialLaplacian.H"
+#include "fvMesh.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace fv
+{
+    makeFvLaplacianScheme(electricPotentialLaplacian)
+}
+}
+
+
+#define declareElectricPotentialLaplacianScalarGamma(Type)                   \
+                                                                             \
+template<>                                                                   \
+Foam::tmp<Foam::fvMatrix<Foam::Type> >                                       \
+Foam::fv::electricPotentialLaplacian<Foam::Type, Foam::scalar>::fvmLaplacian \
+(                                                                            \
+    const GeometricField<scalar, fvsPatchField, surfaceMesh>& gamma,         \
+    GeometricField<Type, fvPatchField, volMesh>& vf                          \
+)                                                                            \
+{                                                                            \
+    const fvMesh& mesh = this->mesh();                                       \
+                                                                             \
+    GeometricField<scalar, fvsPatchField, surfaceMesh> gammaMagSf            \
+    (                                                                        \
+        gamma*mesh.magSf()                                                   \
+    );                                                                       \
+                                                                             \
+    tmp<fvMatrix<Type> > tfvm = fvmLaplacianUncorrected(gammaMagSf, vf);     \
+    fvMatrix<Type>& fvm = tfvm();                                            \
+                                                                             \
+    if (this->tsnGradScheme_().corrected())                                  \
+    {                                                                        \
+        if (mesh.schemesDict().fluxRequired(vf.name()))                      \
+        {                                                                    \
+            fvm.faceFluxCorrectionPtr() = new                                \
+            GeometricField<Type, fvsPatchField, surfaceMesh>                 \
+            (                                                                \
+                gammaMagSf*this->tsnGradScheme_().correction(vf)             \
+            );                                                               \
+                                                                             \
+            fvm.source() -=                                                  \
+                mesh.V()*                                                    \
+                fvc::div                                                     \
+                (                                                            \
+                    *fvm.faceFluxCorrectionPtr()                             \
+                )().internalField();                                         \
+        }                                                                    \
+        else                                                                 \
+        {                                                                    \
+            fvm.source() -=                                                  \
+                mesh.V()*                                                    \
+                fvc::div                                                     \
+                (                                                            \
+                    gammaMagSf*this->tsnGradScheme_().correction(vf)         \
+                )().internalField();                                         \
+        }                                                                    \
+    }                                                                        \
+                                                                             \
+    return tfvm;                                                             \
+}                                                                            \
+                                                                             \
+                                                                             \
+template<>                                                                   \
+Foam::tmp<Foam::GeometricField<Foam::Type, Foam::fvPatchField, Foam::volMesh> >\
+Foam::fv::electricPotentialLaplacian<Foam::Type, Foam::scalar>::fvcLaplacian \
+(                                                                            \
+    const GeometricField<scalar, fvsPatchField, surfaceMesh>& gamma,         \
+    const GeometricField<Type, fvPatchField, volMesh>& vf                    \
+)                                                                            \
+{                                                                            \
+    const fvMesh& mesh = this->mesh();                                       \
+                                                                             \
+    tmp<GeometricField<Type, fvPatchField, volMesh> > tLaplacian             \
+    (                                                                        \
+        fvc::div(gamma*this->tsnGradScheme_().snGrad(vf)*mesh.magSf())       \
+    );                                                                       \
+                                                                             \
+    tLaplacian().rename("laplacian(" + gamma.name() + ',' + vf.name() + ')');\
+                                                                             \
+    return tLaplacian;                                                       \
+}
+
+
+declareElectricPotentialLaplacianScalarGamma(scalar);
+declareElectricPotentialLaplacianScalarGamma(vector);
+declareElectricPotentialLaplacianScalarGamma(sphericalTensor);
+declareElectricPotentialLaplacianScalarGamma(symmTensor);
+declareElectricPotentialLaplacianScalarGamma(tensor);
+
+
+#define declareElectricPotentialLaplacianNamedEnum(Type, GType)              \
+                                                                             \
+template<>                                                                   \
+const char* Foam::NamedEnum                                                  \
+<                                                                            \
+    typename Foam::fv::electricPotentialLaplacian                            \
+    <                                                                        \
+        Foam::Type,                                                          \
+        Foam::GType                                                          \
+    >::baseLaplacianScheme,                                                  \
+    1                                                                        \
+>::names[] =                                                                 \
+{                                                                            \
+    "Gauss"                                                                  \
+}
+
+
+declareElectricPotentialLaplacianNamedEnum(scalar, scalar);
+declareElectricPotentialLaplacianNamedEnum(vector, scalar);
+declareElectricPotentialLaplacianNamedEnum(diagTensor, scalar);
+declareElectricPotentialLaplacianNamedEnum(sphericalTensor, scalar);
+declareElectricPotentialLaplacianNamedEnum(symmTensor, scalar);
+declareElectricPotentialLaplacianNamedEnum(tensor, scalar);
+
+declareElectricPotentialLaplacianNamedEnum(scalar, vector);
+declareElectricPotentialLaplacianNamedEnum(vector, vector);
+declareElectricPotentialLaplacianNamedEnum(diagTensor, vector);
+declareElectricPotentialLaplacianNamedEnum(sphericalTensor, vector);
+declareElectricPotentialLaplacianNamedEnum(symmTensor, vector);
+declareElectricPotentialLaplacianNamedEnum(tensor, vector);
+
+declareElectricPotentialLaplacianNamedEnum(scalar, diagTensor);
+declareElectricPotentialLaplacianNamedEnum(vector, diagTensor);
+declareElectricPotentialLaplacianNamedEnum(diagTensor, diagTensor);
+declareElectricPotentialLaplacianNamedEnum(sphericalTensor, diagTensor);
+declareElectricPotentialLaplacianNamedEnum(symmTensor, diagTensor);
+declareElectricPotentialLaplacianNamedEnum(tensor, diagTensor);
+
+declareElectricPotentialLaplacianNamedEnum(scalar, sphericalTensor);
+declareElectricPotentialLaplacianNamedEnum(vector, sphericalTensor);
+declareElectricPotentialLaplacianNamedEnum(diagTensor, sphericalTensor);
+declareElectricPotentialLaplacianNamedEnum(sphericalTensor, sphericalTensor);
+declareElectricPotentialLaplacianNamedEnum(symmTensor, sphericalTensor);
+declareElectricPotentialLaplacianNamedEnum(tensor, sphericalTensor);
+
+declareElectricPotentialLaplacianNamedEnum(scalar, symmTensor);
+declareElectricPotentialLaplacianNamedEnum(vector, symmTensor);
+declareElectricPotentialLaplacianNamedEnum(diagTensor, symmTensor);
+declareElectricPotentialLaplacianNamedEnum(sphericalTensor, symmTensor);
+declareElectricPotentialLaplacianNamedEnum(symmTensor, symmTensor);
+declareElectricPotentialLaplacianNamedEnum(tensor, symmTensor);
+
+declareElectricPotentialLaplacianNamedEnum(scalar, tensor);
+declareElectricPotentialLaplacianNamedEnum(vector, tensor);
+declareElectricPotentialLaplacianNamedEnum(diagTensor, tensor);
+declareElectricPotentialLaplacianNamedEnum(sphericalTensor, tensor);
+declareElectricPotentialLaplacianNamedEnum(symmTensor, tensor);
+declareElectricPotentialLaplacianNamedEnum(tensor, tensor);
+
+
+// ************************************************************************* //
