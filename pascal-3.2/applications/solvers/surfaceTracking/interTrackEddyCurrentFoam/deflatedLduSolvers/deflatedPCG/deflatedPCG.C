1bff99987154e5dc43185401df888ce4f0fc2b83
Pascal Beckstein <pascal@becksteins.de>
2016-02-11 14:57:20 +0100

    Push submodule to official private version of the foam-extend-3.2 project

--- pascal-3.2/applications/solvers/surfaceTracking/interTrackEddyCurrentFoam/deflatedLduSolvers/deflatedPCG/deflatedPCG.C
+++ pascal-3.2/applications/solvers/surfaceTracking/interTrackEddyCurrentFoam/deflatedLduSolvers/deflatedPCG/deflatedPCG.C
@@ -70,11 +70,7 @@ Foam::lduSolverPerformance Foam::deflatedPCG::solve
 ) const
 {
     // --- Setup class containing solver performance data
-    lduSolverPerformance solverPerf
-    (
-        lduMatrix::preconditioner::getName(dict()) + typeName,
-        fieldName()
-    );
+    lduSolverPerformance solverPerf(typeName, fieldName());
 
     register label nCells = x.size();
 
@@ -126,6 +122,9 @@ Foam::lduSolverPerformance Foam::deflatedPCG::solve
                 interfaces_,
                 dict()
             );
+
+        // Rename the solver pefformance to include precon name
+        solverPerf.solverName() = preconPtr->type() + typeName;
 
         // Solver iteration
         do
