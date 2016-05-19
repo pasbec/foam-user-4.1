e5c5efc4b57e2e3a4e29f4fdb7fcf81d1c7fe3a6
Pascal Beckstein <pascal@becksteins.de>
2016-05-19 18:00:59 +0200

    Update tutorials for eddyCurrentFoam

--- pascal-3.2/tutorials/electromagnetics/eddyCurrentFoam/sphere3D/constant/polyMesh.org/blockMeshDict.py
+++ pascal-3.2/tutorials/electromagnetics/eddyCurrentFoam/sphere3D/constant/polyMesh.org/blockMeshDict.py
@@ -16,9 +16,9 @@ import math as m
 geo_scale = 1e-3
 
 # Background mesh
-geo_r0 =   5.0
-geo_r1 =  10.0
-geo_r2 =  50.0
+geo_r0 =   1.0
+geo_r1 =   2.0
+geo_r2 =  10.0
 
 a = 2.0**(-0.5)
 b = 3.0**( 0.5)/3.0
@@ -32,11 +32,11 @@ geo_b0 = b*geo_r0
 geo_b1 = b*geo_r1
 geo_b2 = b*geo_r2
 
-n_scale = 1.0
+n_scale = 2.0
 
-n0 = int(m.ceil(n_scale*(geo_r0)))
-n1 = int(m.ceil(n_scale*(geo_r1-geo_r0)))
-n2 = int(m.ceil(n_scale*((geo_r2-geo_r1)+(geo_r1-geo_r0))/3.0))
+n0 = int(m.ceil(n_scale*(geo_r0)*10))
+n1 = int(m.ceil(n_scale*(geo_r1-geo_r0)*10))
+n2 = int(m.ceil(n_scale*((geo_r2-geo_r1)+(geo_r1-geo_r0))/6.0*10))
 
 # --------------------------------------------------------------------------- #
 # --- Data ------------------------------------------------------------------ #
@@ -71,30 +71,53 @@ d.vertices.set( 21, [        0,        0,   geo_r2])
 
 # Blocks
 
-d.blocks.set( 0, [  0,  1,  2,  3,  4,  5,  6,  7], [n0, n0, n0], zone="conductor")
+d.blocks.set( 0, [  0,  1,  2,  3,  4,  5,  6,  7], zone="conductor")
 
-d.blocks.set( 1, [  1,  8,  9,  2,  5, 11, 12,  6], [n1, n1, n1], zone="conductor")
-d.blocks.set( 2, [  3,  2,  9, 10,  7,  6, 12, 13], [n1, n1, n1], zone="conductor")
-d.blocks.set( 3, [  4,  5,  6,  7, 14, 11, 12, 13], [n1, n1, n1], zone="conductor")
+d.blocks.set( 1, [  1,  8,  9,  2,  5, 11, 12,  6], zone="conductor")
+d.blocks.set( 2, [  3,  2,  9, 10,  7,  6, 12, 13], zone="conductor")
+d.blocks.set( 3, [  4,  5,  6,  7, 14, 11, 12, 13], zone="conductor")
 
-d.blocks.set( 4, [  8, 15, 16,  9, 11, 18, 19, 12], [n2, n1, n1], zone="background")
-d.blocks.set( 5, [ 10,  9, 16, 17, 13, 12, 19, 20], [n1, n2, n1], zone="background")
-d.blocks.set( 6, [ 14, 11, 12, 13, 21, 18, 19, 20], [n1, n1, n2], zone="background")
-
-# Gradings
-
-d.blocks.grading.set( 4, [5.0, 1.0, 1.0])
+d.blocks.set( 4, [  8, 15, 16,  9, 11, 18, 19, 12], zone="background")
+d.blocks.set( 5, [ 10,  9, 16, 17, 13, 12, 19, 20], zone="background")
+d.blocks.set( 6, [ 14, 11, 12, 13, 21, 18, 19, 20], zone="background")
 
 # Distributions
 
+d.blocks.distribution.set(0, "x", n0)
+d.blocks.distribution.set(0, "y", n0)
+d.blocks.distribution.set(0, "z", n0)
+
+d.blocks.distribution.set(1, "x", n1)
+
+d.blocks.distribution.set(4, "x", n2)
+
+# Gradings
+
+d.blocks.grading.set( 4, [8.0, 1.0, 1.0])
+
 # Boundary faces
 
-#d.boundaryFaces.set(1, "infinity", 0, "y-")
-#d.boundaryFaces.set(2, "infinity", 0, "y+")
-#d.boundaryFaces.set(3, "infinity", 0, "x-")
-#d.boundaryFaces.set(4, "infinity", 0, "x+")
-#d.boundaryFaces.set(5, "infinity", 0, "z-")
-#d.boundaryFaces.set(6, "infinity", 0, "z+")
+d.boundaryFaces.set( 0, "mirror_x", 0, "x-")
+d.boundaryFaces.set( 1, "mirror_x", 2, "x-")
+d.boundaryFaces.set( 2, "mirror_x", 3, "x-")
+d.boundaryFaces.set( 3, "mirror_x", 5, "x-")
+d.boundaryFaces.set( 4, "mirror_y", 6, "x-")
+
+d.boundaryFaces.set( 5, "mirror_y", 0, "y-")
+d.boundaryFaces.set( 6, "mirror_y", 1, "y-")
+d.boundaryFaces.set( 7, "mirror_y", 3, "y-")
+d.boundaryFaces.set( 8, "mirror_y", 4, "y-")
+d.boundaryFaces.set( 9, "mirror_y", 6, "y-")
+
+d.boundaryFaces.set(10, "mirror_z", 0, "z-")
+d.boundaryFaces.set(11, "mirror_z", 1, "z-")
+d.boundaryFaces.set(12, "mirror_z", 2, "z-")
+d.boundaryFaces.set(13, "mirror_z", 4, "z-")
+d.boundaryFaces.set(14, "mirror_z", 5, "z-")
+
+d.boundaryFaces.set(15, "infinity", 4, "x+")
+d.boundaryFaces.set(16, "infinity", 5, "y+")
+d.boundaryFaces.set(17, "infinity", 6, "z+")
 
 # --------------------------------------------------------------------------- #
 # --- blockMeshDict --------------------------------------------------------- #
@@ -142,6 +165,18 @@ if d.subDict("edges"):
 
 
 if d.subDict("boundary"):
+
+    if d.boundarySubDict("mirror_x", "patch"):
+
+        d.boundaryFaces.write()
+
+    if d.boundarySubDict("mirror_y", "patch"):
+
+        d.boundaryFaces.write()
+
+    if d.boundarySubDict("mirror_z", "patch"):
+
+        d.boundaryFaces.write()
 
     if d.boundarySubDict("infinity", "patch"):
 
